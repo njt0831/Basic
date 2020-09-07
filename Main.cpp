cf6c017642504553de8795325ed7eb0b3e941bba
njt0831 <njt0831@gmail.com>
2020-09-07 18:50:40 -0400

    Main() changes

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,66 @@
 #include "Basic.hpp"
+#include "X11/Xlib.h"
+
 
 int main(){
 
-	printf("henlo");
+	Basic basic = Basic::Create();
+	basic.Initialize();
 
-	Basic windowManager = Basic::Create();
-	windowManager.Mainloop();
+	while (1){
+		
+		XNextEvent(basic.display_, &basic.event_);
+
+		switch (event_.type){
+
+			case ConfigureRequest:
+				
+				basic.handleConfigureRequest();
+				break;
+
+			case MapRequest:
+
+				basic.handleMapRequest();
+				break;
+
+			case UnmapNotify:
+
+				basic.handleUnmapNotify();
+				break;
+
+			case ButtonPress:
+
+				basic.handleButtonPress();
+				break;
+
+			case ButtonRelease:
+
+				basic.handleButtonRelease();
+				break;
+
+			case MotionNotify:
+
+				basic.handleMotionNotify();
+				break;
+
+			case EnterNotify:
+
+				basic.handleEnterNotify();
+				break;
+
+			case LeaveNotify:
+
+				basic.handleLeaveNotify();
+				break;
+
+	 		default:
+
+				break;
+
+		}
+
+		basic.Action();
+	
+	}
 
 }
