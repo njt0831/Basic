fd7ebb800e889d987204648d0b9abcdc1adf5949
njt0831 <njt0831@gmail.com>
2020-09-08 01:20:04 -0400

    refactoring....

--- source/handlers/mapRequest.cpp
+++ source/handlers/mapRequest.cpp
@@ -2,107 +2,105 @@
 
 void Basic::handleMapRequest(XMapRequestEvent ev){
 
-	if (clientFrame_.count(event_.xmaprequest.window)){
-					
-		break;
+	if (client-frame_.count(ev.xmaprequest.window)){
+			
+		return;
 				
 	}
 
-	XGetWMName(display_, event_.xmaprequest.window, &textProp);
-	XGetClassHint(display_, event_.xmaprequest.window, &winClass);
-	XGetWindowAttributes(display_, event_.xmaprequest.window, &winAtt);
+	XGetWMName(display_, ev.xmaprequest.window, &tempTextProperty);
+	XGetClassHint(display_, ev.xmaprequest.window, &tempClassHint);
+	XGetWindowAttributes(display_, event_.xmaprequest.window, &tempWindowAttributes);
 				
-	hints = XGetWMHints(display_, event_.xmaprequest.window);
+	tempWMHints = XGetWMHints(display_, ev.xmaprequest.window);
+
+	if (!tempWMHints){
+		
+		XMapWindow(display_, ev.xmaprequest.window);
+		return;
+	}
+				
 	
-
-	if (!hints){
-					
-		XMapWindow(display_, event_.xmaprequest.window);
-		break;
-					
-	}
-					
-
 	// Fix this when it matters
 	// Some clients wont want to be parented so they dont set their WM_NAME property			
-	if (textProp.value == NULL){
+	if (tempTextProperty.value == NULL){
 
-		if (winClass.res_name == NULL){
+		if (tempClassHint.res_name == NULL){
 
-			winClass.res_name = (char*) "";
+			tempClassHint.res_name = (char*) "";
 					
 		}else{
 			// Just set any Null textprop to spotify
 
 			unsigned char* fuckspotify = (unsigned char*) "spotify";
-			textProp.value = (unsigned char*) fuckspotify;
-			textProp.encoding = XInternAtom(display_, "WM_NAME", true);
-			textProp.nitems = 8;
-			textProp.format = 8;
-					
+			tempTextProperty.value = (unsigned char*) fuckspotify;
+			tempTextProperty.encoding = XInternAtom(display_, "WM_NAME", true);
+			tempTextProperty.nitems = 8;
+			tempTextProperty.format = 8;
+			
 		}
 				
-	}else if (!(strcmp((char*)textProp.value, "Mozilla Firefox"))){
+	}else if (!(strcmp((char*)tempTextProperty.value, "Mozilla Firefox"))){
 				
 		unsigned char* fuckspotify = (unsigned char*) "firefox";
-		textProp.value = (unsigned char*) fuckspotify;
-		textProp.encoding = XInternAtom(display_, "WM_NAME", true);
-		textProp.nitems = 8;
-		textProp.format = 8;
-					
+		tempTextProperty.value = (unsigned char*) fuckspotify;
+		tempTextProperty.encoding = XInternAtom(display_, "WM_NAME", true);
+		tempTextProperty.nitems = 8;
+		tempTextProperty.format = 8;
+			
 	}
 
-	if (!(strcmp((char*)textProp.value, "Steam")) || !(strcmp((char*)textProp.value, "Rocket League"))){
+	if (!(strcmp((char*)tempTextProperty.value, "Steam")) || !(strcmp((char*)tempTextProperty.value, "Rocket League"))){
 					
-		XMapWindow(display_, event_.xmaprequest.window);
-		break;
+		XMapWindow(display_, ev.xmaprequest.window);
+		return;
 	
 	}
-				
+	
 ////////////////// Figure out how clients tell the window manager they dont want a frame so you can delete the shit above this //////////////////////////////
 
-	if (winAtt.x < 0 || winAtt.y < 0 || winAtt.y > 1080 or winAtt.x > 1920){
+	if (tempWindowAttributes.x < 0 || tempWindowAttributes.y < 0 || tempWindowAttributes.y > 1080 or tempWindowAttributes.x > 1920){
 					
-		XMoveWindow(display_, event_.xmaprequest.window, 0, 0);
-		winAtt.x = 0; 
-		winAtt.y = 0;
+		XMoveWindow(display_, ev.xmaprequest.window, 0, 0);
+		tempWindowAttributes.x = 0; 
+		tempWindowAttributes.y = 0;
 	
 	}
 
-	newFrame = XCreateSimpleWindow(display_, root_, winAtt.x, winAtt.y, winAtt.width, winAtt.height + FRAME_TITLE_BAR_WIDTH, FRAME_BORDER_WIDTH, FRAME_BORDER_COLOR, FRAME_COLOR);
-	closeButton = XCreateSimpleWindow(display_, root_, 0, 0, 20, 12, 1, 0x181616, 0x363333);
-	minimizeButton = XCreateSimpleWindow(display_, root_, 0, 0, 20, 12, 1, 0x181616, 0x363333);	
+	tempWindowFrame = XCreateSimpleWindow(display_, root_, tempWindowAttributes.x, tempWindowAttributes.y, tempWindowAttributes.width, tempWindowAttributes.height + FRAME_TITLE_BAR_WIDTH, FRAME_BORDER_WIDTH, FRAME_BORDER_COLOR, FRAME_COLOR);
+	tempWindowClose = XCreateSimpleWindow(display_, root_, 0, 0, 20, 12, 1, 0x181616, 0x363333);
+	tempWindowMinimize = XCreateSimpleWindow(display_, root_, 0, 0, 20, 12, 1, 0x181616, 0x363333);	
 	
-	XSelectInput(display_, newFrame, SubstructureRedirectMask | SubstructureNotifyMask | ButtonPressMask | ButtonReleaseMask | ButtonMotionMask);
-	XSelectInput(display_, closeButton, ButtonPressMask);		
-	XSelectInput(display_, minimizeButton, ButtonPressMask);
+	XSelectInput(display_, tempWindowFrame, SubstructureRedirectMask | SubstructureNotifyMask | ButtonPressMask | ButtonReleaseMask | ButtonMotionMask);
+	XSelectInput(display_, tempWindowClose, ButtonPressMask);		
+	XSelectInput(display_, tempWindowMinimize, ButtonPressMask);
+	
+	XReparentWindow(display_, ev.xmaprequest.window, tempWindowFrame, 0, FRAME_TITLE_BAR_WIDTH);
+	XReparentWindow(display_, tempWindowClose, tempWindowFrame, tempWindowAttributes.width - 25, 4);	
+	XReparentWindow(display_, tempWindowMinimize, tempWindowFrame, winAtt.width - 50, 4);
 
-	XReparentWindow(display_, event_.xmaprequest.window, newFrame, 0, FRAME_TITLE_BAR_WIDTH);
-	XReparentWindow(display_, closeButton, newFrame, winAtt.width - 25, 4);	
-	XReparentWindow(display_, minimizeButton, newFrame, winAtt.width - 50, 4);
-
-	XMapWindow(display_, newFrame);	
-	XMapWindow(display_, closeButton);
-	XMapWindow(display_, minimizeButton);
-	XMapWindow(display_, event_.xmaprequest.window);
-	XMoveWindow(display_, event_.xmaprequest.window, 0, FRAME_TITLE_BAR_WIDTH); 
-
-	LoadResource("/root/Basic/resources/utilities/close.png", closeButton, 20, 12);
+	XMapWindow(display_, tempWindowFrame);
+	XMapWindow(display_, tempWindowClose);
+	XMapWindow(display_, tempWindowMinimize);
+	XMapWindow(display_, ev.xmaprequest.window);
+	XMoveWindow(display_, ev.xmaprequest.window, 0, FRAME_TITLE_BAR_WIDTH);
+	
+	LoadResource("/root/Basic/resources/utilities/close.png", tempWindowClose, 20, 12);
 	// Load Minimize button resource here
 
-	XSetWMName(display_, newFrame, &textProp);
-
-	setWinAtt.backing_store = 2;
-	XChangeWindowAttributes(display_, newFrame, CWBackingStore, &setWinAtt);
-	XDrawString(display_, newFrame, XDefaultGC(display_, DefaultScreen(display_)), FRAME_BORDER_WIDTH, (int) FRAME_TITLE_BAR_WIDTH / 2, (char*) textProp.value, strlen((char*) textProp.value));
+	XSetWMName(display_, tempWindowFrame, &tempTextProperty);
 	
-	clientFrame_[event_.xmaprequest.window] = newFrame;
-	frameClient_[newFrame] = event_.xmaprequest.window;
-	killClient_[closeButton] = event_.xmaprequest.window;
-	frameKill_[newFrame] = closeButton;
-	minClient_[minimizeButton] = event_.xmaprequest.window;
+	tempSetWindowAttributes.backing_store = 2;
+	XChangeWindowAttributes(display_, tempWindowFrame, CWBackingStore, &tempSetWindowAttributes);
+	XDrawString(display_, newFrame, XDefaultGC(display_, DefaultScreen(display_)), FRAME_BORDER_WIDTH, (int) FRAME_TITLE_BAR_WIDTH / 2, (char*) tempTextProperty.value, strlen((char*) tempTextProperty.value));
+
+	client-frame_[ev.xmaprequest.window] = tempWindowFrame;
+	frame-client_[tempWindowFrame] = ev.xmaprequest.window;
+	close-client_[tempWindowClose] = ev.xmaprequest.window;
+	frame-close_[tempWindowFrame] = tempWindowClose;
+	minimize-client_[tempWindowMinimize] = ev.xmaprequest.window;
 				
 	// Without this you dont get button events when clicking clients. Necessitates an XAllowEvents in the button press loop to forward the events to the clients. Eventually will want to grab the keyboard as well to allow for the addition of general keyboard shortcuts.  
-	XGrabButton(display_, AnyButton, AnyModifier, newFrame, false, ButtonPressMask | ButtonReleaseMask, GrabModeSync, GrabModeAsync, None, None);
+	XGrabButton(display_, AnyButton, AnyModifier, tempWindowFrame, false, ButtonPressMask | ButtonReleaseMask, GrabModeSync, GrabModeAsync, None, None);
 
 }
