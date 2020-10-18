2633ab2bd8e1480dd642b1e02d41bf8cfdef0329
njt0831 <njt0831@gmail.com>
2020-10-18 01:46:36 -0400

    Fixed incorrect client resizing

--- source/handlers/motionNotify.cpp
+++ source/handlers/motionNotify.cpp
@@ -12,14 +12,14 @@ void Basic::handleMotionNotify(XPointerMovedEvent ev){
 			// And the bottom (i.e. corner)
 			if (resizeDown){
 				
-				XResizeWindow(display_, frame_client_[ev.window], hookWidth + (ev.x - hookXOffset), hookHeight + (ev.y - hookYOffset));
+				XResizeWindow(display_, frame_client_[ev.window], hookWidth + (ev.x - hookXOffset), hookHeight + (ev.y - hookYOffset) - FRAME_TITLE_BAR_WIDTH + 2 * FRAME_BORDER_WIDTH);
 				XMoveWindow(display_, client_minimize_[frame_client_[ev.window]], hookWidth + (ev.x - hookXOffset) - 50, 4);
 				XMoveWindow(display_, frame_close_[ev.window], hookWidth + (ev.x - hookXOffset) - 25, 4);	
 				XResizeWindow(display_, ev.window, hookWidth + (ev.x - hookXOffset), hookHeight + (ev.y - hookYOffset));
 				
 			}else{
 				
-				XResizeWindow(display_, frame_client_[ev.window], hookWidth + (ev.x - hookXOffset), hookHeight);
+				XResizeWindow(display_, frame_client_[ev.window], hookWidth + (ev.x - hookXOffset), hookHeight - FRAME_TITLE_BAR_WIDTH + 2 * FRAME_BORDER_WIDTH);
 				XMoveWindow(display_, client_minimize_[frame_client_[ev.window]], hookWidth + (ev.x - hookXOffset) - 50, 4);
 				XMoveWindow(display_, frame_close_[ev.window], hookWidth + (ev.x - hookXOffset) - 25, 4);	
 				XResizeWindow(display_, ev.window, hookWidth + (ev.x - hookXOffset), hookHeight);
